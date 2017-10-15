d1682b6cdc9e7897eaedb11b866ce7dfaf4b5f14
kalinaeva <masha.kalinaeva@gmail.com>
2017-10-15 23:52:11 +0300

    calculator updated

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -10,19 +10,43 @@ public class Base {
          * считывает два дробных числа и выводит их сумму с точностью до 4ого знака
          */
         Scanner scanner = new Scanner(System.in);
+        int w = scanner.nextInt();
+        if (w == 1) {
 
 
-        System.out.println("Введите первое число: ");
-        double a = scanner.nextDouble();
+            System.out.println("Введите первое число: ");
+            double a = scanner.nextDouble();
 
-        System.out.println("Введите второе число: ");
-        double b = scanner.nextDouble();
+            System.out.println("Введите второе число: ");
+            double b = scanner.nextDouble();
 
-        double sum = a + b;
+            System.out.println("Введите операцию (+,-,*,/): ");
+            String oper = scanner.next();
 
-        System.out.print("Sum: ");
-        System.out.printf("%10.4f", sum);
+            if (oper.equals("+")) {
+                double c = a + b;
+                System.out.print("Сумма: ");
+                System.out.printf("%10.4f", c);
+            } else if (oper.equals("-")) {
+                double c = a - b;
+                System.out.print("Разность: ");
+                System.out.printf("%10.4f", c);
+            } else if (oper.equals("*")) {
+                double c = a * b;
+                System.out.print("Умножение: ");
+                System.out.printf("%10.4f", c);
+            } else if (oper.equals("/")) {
+                double c = a / b;
+                System.out.print("Деление: ");
+                System.out.printf("%10.4f", c);
+            } else {
+                System.out.print("Недопустимая операция");
+            }
+        }
 
+        else if (w == 2){
+            System.out.println("Тут будет сортировка");
+        }
         scanner.close();
 
     }
